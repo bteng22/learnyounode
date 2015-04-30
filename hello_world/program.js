cb6d11767df1c15fa5672428eb3bf26c8882a15c
bteng22 <bteng22@gmail.com>
2015-04-29 23:22:25 -0400

    implement hello_world & baby_steps

---
+++ hello_world/program.js
@@ -0,0 +1,11 @@
+// var express = require('express');
+// var app = express();
+
+// // app.get('/', function(req, res) {
+// //   console.log("Hello World!");
+// // })
+
+// var port = process.env.PORT || 8080;
+
+// app.listen(port);
+console.log("HELLO WORLD");
